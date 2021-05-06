6d74afd054f6d570202f187b17f221059bae0000
raogaru <raogaruinc@gmail.com>
2021-05-05 23:09:03 -0400

    Team_Gate_Pipeline.groovy

--- setup/Main_Gate_Pipeline.groovy
+++ setup/Main_Gate_Pipeline.groovy
@@ -115,13 +115,13 @@ pipeline {
 
 		stage('System-Gate-Exit') { steps { build 'DEMO-CI-590-System-Gate-Exit' } }
 
-		stage('Relesae-Gate-Entry') { steps { build 'DEMO-CI-600-Relesae-Gate-Entry' } }
-		stage('Relesae-Gate-Prepare') { steps { build 'DEMO-CI-610-Relesae-Gate-Prepare' } }
-		stage('Relesae-Gate-Build') { steps { build 'DEMO-CI-620-Relesae-Gate-Build' } }
-		stage('Relesae-Gate-Verfiy') { steps { build 'DEMO-CI-630-Relesae-Gate-Verfiy' } }
-		stage('Relesae-Gate-Publish') { steps { build 'DEMO-CI-640-Relesae-Gate-Publish' } }
-		stage('Relesae-Gate-Notify') { steps { build 'DEMO-CI-650-Relesae-Gate-Notify' } }
-		stage('Relesae-Gate-Exit') { steps { build 'DEMO-CI-690-Relesae-Gate-Exit' } }
+		stage('Release-Gate-Entry') { steps { build 'DEMO-CI-600-Release-Gate-Entry' } }
+		stage('Release-Gate-Prepare') { steps { build 'DEMO-CI-610-Release-Gate-Prepare' } }
+		stage('Release-Gate-Build') { steps { build 'DEMO-CI-620-Release-Gate-Build' } }
+		stage('Release-Gate-Verfiy') { steps { build 'DEMO-CI-630-Release-Gate-Verfiy' } }
+		stage('Release-Gate-Publish') { steps { build 'DEMO-CI-640-Release-Gate-Publish' } }
+		stage('Release-Gate-Notify') { steps { build 'DEMO-CI-650-Release-Gate-Notify' } }
+		stage('Release-Gate-Exit') { steps { build 'DEMO-CI-690-Release-Gate-Exit' } }
 
 		stage('Exit') { steps { build 'DEMO-CI-990-Main-Gate-Exit' } }
 	}
