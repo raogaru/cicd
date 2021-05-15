30d007e82c28f9e39a711658e3354cf19a9a92d2
raogaru <raogaruinc@gmail.com>
2021-05-15 13:52:29 -0400

    cicd

--- build_db.sh
+++ build_db.sh
@@ -1,7 +1,10 @@
-ECHOpurple "script:build_liquibase.sh START"
+ECHOpurple "script:build_db.sh START"
 ECHO "Drop and recreate database for team ${v_team}"
 
-ECHO "Prepare ${PIPE_DIR}/build_db.sql" 
+BUILD_DB_SQL=${PIPE_DIR}/build_db_${v_team}.sql
+#ADDENV "BUILD_DB_SQL_${v_team}=${BUILD_DB_SQL}"
+
+ECHO "Prepare ${BUILD_DB_SQL}" 
 echo "
 \list
 drop database ${v_team};
@@ -13,12 +16,13 @@ create database ${v_team};
 create schema ${MYAPP_NAME};
 \dn
 set search_path to ${MYAPP_NAME};
-" >> ${PIPE_DIR}/build_db.sql
+" > ${BUILD_DB_SQL}
 
-HEADER2 "File: ${PIPE_DIR}/build_db.sql"
-cat ${PIPE_DIR}/build_db.sql
+HEADER2 "File: ${BUILD_DB_SQL}"
+cat ${BUILD_DB_SQL}
 
 HEADER2 "Execute ${PIPE_DIR}/build_db.sql"
-psql -h localhost -p 5432 -d rao -U rao -P rao -f ${PIPE_DIR}/build_db.sql
+export PGPASSWORD=rao
+psql -h localhost -p 5432 -d rao -U rao -f ${BUILD_DB_SQL}
 
-ECHOpurple "script:build_liquibase.sh END"
+ECHOpurple "script:build_db.sh END"
