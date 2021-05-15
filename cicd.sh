e3379b6426d8c089b052ed7d7a3186498065c3cf
raogaru <raogaruinc@gmail.com>
2021-05-15 13:05:27 -0400

    cicd

--- cicd.sh
+++ cicd.sh
@@ -15,88 +15,88 @@ case "${ARG1}" in
 	. ${WORKSPACE}/Main_Gate_Entry.sh
 	;;
 "Team-Gate-Entry") 
-	. ./Team_Gate_Entry.sh
+	. ${WORKSPACE}/Team_Gate_Entry.sh
 	;;
 "Team-Build-MARS-1") 
-	. ./Team_Gate_Build.sh mars jar
+	. ${WORKSPACE}/Team_Gate_Build.sh mars jar
 	;;
 "Team-Build-VENUS-1") 
-	. ./Team_Gate_Build.sh venus jar
+	. ${WORKSPACE}/Team_Gate_Build.sh venus jar
 	;;
 "Team-Build-PLUTO-1") 
-	. ./Team_Gate_Build.sh pluto jar
+	. ${WORKSPACE}/Team_Gate_Build.sh pluto jar
 	;;
 "Team-Build-MARS-2") 
-	. ./Team_Gate_Build.sh mars docker
+	. ${WORKSPACE}/Team_Gate_Build.sh mars docker
 	;;
 "Team-Build-VENUS-2") 
-	. ./Team_Gate_Build.sh venus docker
+	. ${WORKSPACE}/Team_Gate_Build.sh venus docker
 	;;
 "Team-Build-PLUTO-2") 
-	. ./Team_Gate_Build.sh pluto docker
+	. ${WORKSPACE}/Team_Gate_Build.sh pluto docker
 	;;
 "Team-Build-MARS-3") 
-	. ./Team_Gate_Build.sh mars ec2
+	. ${WORKSPACE}/Team_Gate_Build.sh mars ec2
 	;;
 "Team-Build-VENUS-3") 
-	. ./Team_Gate_Build.sh venus ec2
+	. ${WORKSPACE}/Team_Gate_Build.sh venus ec2
 	;;
 "Team-Build-PLUTO-3") 
-	. ./Team_Gate_Build.sh pluto ec2
+	. ${WORKSPACE}/Team_Gate_Build.sh pluto ec2
 	;;
 "Team-Deploy-MARS-1") 
-	. ./Team_Gate_Deploy.sh mars liquibase
+	. ${WORKSPACE}/Team_Gate_Deploy.sh mars liquibase
 	;;
 "Team-Deploy-VENUS-1") 
-	. ./Team_Gate_Deploy.sh venus liquibase
+	. ${WORKSPACE}/Team_Gate_Deploy.sh venus liquibase
 	;;
 "Team-Deploy-PLUTO-1") 
-	. ./Team_Gate_Deploy.sh pluto liquibase
+	. ${WORKSPACE}/Team_Gate_Deploy.sh pluto liquibase
 	;;
 "Team-Deploy-MARS-2") 
-	. ./Team_Gate_Deploy.sh mars kubernetes
+	. ${WORKSPACE}/Team_Gate_Deploy.sh mars kubernetes
 	;;
 "Team-Deploy-VENUS-2") 
-	. ./Team_Gate_Deploy.sh venus kubernetes
+	. ${WORKSPACE}/Team_Gate_Deploy.sh venus kubernetes
 	;;
 "Team-Deploy-PLUTO-2") 
-	. ./Team_Gate_Deploy.sh pluto kubernetes
+	. ${WORKSPACE}/Team_Gate_Deploy.sh pluto kubernetes
 	;;
 "Team-Deploy-MARS-3") 
-	. ./Team_Gate_Deploy.sh mars tomcat
+	. ${WORKSPACE}/Team_Gate_Deploy.sh mars tomcat
 	;;
 "Team-Deploy-VENUS-3") 
-	. ./Team_Gate_Deploy.sh venus tomcat
+	. ${WORKSPACE}/Team_Gate_Deploy.sh venus tomcat
 	;;
 "Team-Deploy-PLUTO-3") 
-	. ./Team_Gate_Deploy.sh pluto tomcat
+	. ${WORKSPACE}/Team_Gate_Deploy.sh pluto tomcat
 	;;
 "Team-Test-MARS-1") 
-	. ./Team_Gate_Test.sh mars phase1
+	. ${WORKSPACE}/Team_Gate_Test.sh mars phase1
 	;;
 "Team-Test-VENUS-1") 
-	. ./Team_Gate_Test.sh venus phase1
+	. ${WORKSPACE}/Team_Gate_Test.sh venus phase1
 	;;
 "Team-Test-PLUTO-1") 
-	. ./Team_Gate_Test.sh pluto phase1
+	. ${WORKSPACE}/Team_Gate_Test.sh pluto phase1
 	;;
 "Team-Test-MARS-2") 
-	. ./Team_Gate_Test.sh mars phase2
+	. ${WORKSPACE}/Team_Gate_Test.sh mars phase2
 	;;
 "Team-Test-VENUS-2") 
-	. ./Team_Gate_Test.sh venus phase2
+	. ${WORKSPACE}/Team_Gate_Test.sh venus phase2
 	;;
 "Team-Test-PLUTO-2") 
-	. ./Team_Gate_Test.sh pluto phase2
+	. ${WORKSPACE}/Team_Gate_Test.sh pluto phase2
 	;;
 "Team-Test-MARS-3") 
-	. ./Team_Gate_Test.sh mars phase3
+	. ${WORKSPACE}/Team_Gate_Test.sh mars phase3
 	;;
 "Team-Test-VENUS-3") 
-	. ./Team_Gate_Test.sh venus phase3
+	. ${WORKSPACE}/Team_Gate_Test.sh venus phase3
 	;;
 "Team-Test-PLUTO-3") 
-	. ./Team_Gate_Test.sh pluto phase3
+	. ${WORKSPACE}/Team_Gate_Test.sh pluto phase3
 	;;
 "Team-Gate-Exit") 
 	ECHOpurple "Option:${ARG1}"
