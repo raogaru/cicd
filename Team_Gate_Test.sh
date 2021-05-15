c0e4c50648e97b1cac392913040331b768eb35f0
raogaru <raogaruinc@gmail.com>
2021-05-15 10:01:55 -0400

    cicd

--- Team_Gate_Test.sh
+++ Team_Gate_Test.sh
@@ -4,25 +4,24 @@ ECHOpurple "script:Team_Gate_Test.sh argument:$1 $2 START"
 v_team=${1}	# team name
 v_type=${2}	# test type
 
-HEADER2 "Build \"${v_type}\" in team-${v_team} branch requested"
+HEADER2 "Test \"${v_type}\" in team-${v_team} branch requested"
 
-HEADER2 "Check if commits in team-${v_team} branch"
+HEADER2 "Check if deploy in team-${v_team} branch successful"
 
 v_deployed=$(READENV TEAM_DEPLOY_${v_team})
 
-if [ "${v_deployed}" != "YES" ]; then
+# ----------------------------------------------------------------------
+f_teamgate_test_val_build () {
+if [ "${v_deployed}" != "SUCCESS" ]; then
 	ECHO "Deploy not scucess. Nothing to do."
+	return 1
 else
 	ECHO "Proceed with \"${v_type}\" test for team ${v_team} ..."
 fi
-# ----------------------------------------------------------------------
-f_teamgate_test () {
-
-HEADER2 "team gate test "
 }
 # ----------------------------------------------------------------------
-f_teamgate_test
-
+f_teamgate_test () {
+HEADER2 "team gate test "
 case "${v_type}" in
 "phase1") 
 	HEADER2 "test phase1" 
@@ -40,7 +39,12 @@ case "${v_type}" in
 	ADDENV "TEAM_TEST_${v_team}=SUCCESS"
 	;;
 esac
-
+}
+# ----------------------------------------------------------------------
+f_teamgate_test_val_build
+if [ $? -eq 0 ]; then 
+	f_teamgate_test
+fi
 # ######################################################################
 ECHOpurple "script:Team_Gate_Test.sh END"
 # ######################################################################
