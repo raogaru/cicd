7d275053b359cb98691fdeea5bd5a7b9d1f9d74c
raogaru <raogaruinc@gmail.com>
2021-05-15 12:32:39 -0400

    cicd

---
+++ build_docker.sh
@@ -0,0 +1,3 @@
+ECHOyello "script:build_docker.sh START"
+ECHOyello "building docker image"
+ECHOyello "script:build_docker.sh END"
---
+++ build_jar.sh
@@ -0,0 +1,3 @@
+ECHOyello "script:build_jar.sh START"
+ECHOyello "building jar file"
+ECHOyello "script:build_jar.sh END"
