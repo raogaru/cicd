3dbf33391d9442ed6e0f5d2402c13defe57e9d57
raogaru <raogaruinc@gmail.com>
2021-05-16 19:07:46 -0400

    cicd

---
+++ Release_Gate_Build.sh
@@ -0,0 +1,12 @@
+# ######################################################################
+ECHOpurple "script:Release_Gate_Build.sh argument:$1 $2 START"
+# ######################################################################
+
+HEADER2 "Prepare Release Number"
+
+ECHO "Release Number: ${REL_MAJOR_NUM}.${REL_MINOR_NUM}.${PIPE_NUM}"
+
+
+# ######################################################################
+ECHOpurple "script:Release_Gate_Build.sh END"
+# ######################################################################
