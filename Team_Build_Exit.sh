7c8dc85fd0fbcdf278447d96f814fb828177f1b4
raogaru <raogaruinc@gmail.com>
2021-05-16 09:45:56 -0400

    cicd

--- Team_Build_Exit.sh
+++ Team_Build_Exit.sh
@@ -3,14 +3,14 @@ ECHOpurple "script:Team_Build_Exit.sh.sh START"
 # ######################################################################
 for TEAM in ${AGILE_TEAMS}
 do
-	ECHO "Evaluate team build status for ${TEAM}"
+	HEADER2 "Evaluate team build status for ${TEAM}"
 	v_build_final="SUCCESS"
 	for PHASE in ${TEAM_BUILD_PHASES}
 	do
 		v_build=$(READENV TEAM_BUILD_${TEAM}_${PHASE})
+		ECHO "TEAM_BUILD_${TEAM}_${PHASE}=${v_build}"
 		[[ "${v_build}" == "FAILED" ]] && v_build_final="FAILED"
 		[[ "${v_build}" == "N/A" ]] && v_build_final="N/A"
-		ECHO "TEAM_BUILD_${TEAM}_${PHASE}=${v_build}"
 	done
 	ADDENV "TEAM_BUILD_${TEAM}=${v_build_final}"
 done
