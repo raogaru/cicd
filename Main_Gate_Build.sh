5248f433455ac3e38d16b14de853c1554b7b9c80
raogaru <raogaruinc@gmail.com>
2021-05-22 07:13:22 -0400

    cicd

--- Main_Gate_Build.sh
+++ Main_Gate_Build.sh
@@ -2,17 +2,22 @@
 FileMarker "script:Main_Gate_Build.sh START"
 # ######################################################################
 
+v_docker_build_final_status="${cPASS}
+v_docker_build_final_point=""
 for v_docker in ${DOCKER_LIST}
 do
 	HEADER2 "DOCKER BUILD ${v_docker}"
 	docker build -t rao-${v_docker} docker/${v_docker} | pipe ${PIPE_DIR}/docker_build_${v_docker}.log
 	if [ $? -eq 0 ]; then
-		ADDENV "DOCKER_BUILD_${v_docker}=PASS"
+		ADDENV "DOCKER_BUILD_${v_docker}=${cPASS}"
 	else
-		ADDENV "DOCKER_BUILD_${v_docker}=FAIL"
-	 	ERROR "docker build failed for \"${v_docker}\""
+		ADDENV "DOCKER_BUILD_${v_docker}=${cFAIL}"
+		v_docker_build_final_status=${cFAIL}
+		v_docker_build_final_point=${v_docker}
 	fi
 done
+
+[[ "${v_docker_build_final_status}" == "${cFAIL}" ]] && ERROR "docker build failed. check ${PIPE_DIR}/docker_build_\*.log"
 
 # ######################################################################
 FileMarker "script:Main_Gate_Build.sh END"
