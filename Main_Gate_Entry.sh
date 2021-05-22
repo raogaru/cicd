d314a86307f1dc956e7b971f6534ea79357ad7d8
raogaru <raogaruinc@gmail.com>
2021-05-21 23:47:20 -0400

    cicd

--- Main_Gate_Entry.sh
+++ Main_Gate_Entry.sh
@@ -35,9 +35,11 @@ ECHO "Workspace directory is ${WORKSPACE}"
 
 HEADER2 "Build tomcat docker"
 docker build -t rao-tomcat docker/tomcat
+[[ $? -ne 0 ]] && ERROR "docker build failed for \"tomcat\""
 
 HEADER2 "Build postgres docker"
 docker build -t rao-postgres docker/postgres
+[[ $? -ne 0 ]] && ERROR "docker build failed for \"postgres\""
 
 echo "script:Main_Gate_Entry.sh END"
 # ######################################################################
