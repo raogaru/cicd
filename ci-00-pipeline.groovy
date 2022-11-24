e720b0f18a1c93b168ab46703cc6ccadad0500e5
raogaru <raogaruinc@gmail.com>
2022-11-24 14:07:33 -0500

    Create ci-00-pipeline.groovy

---
+++ ci-00-pipeline.groovy
@@ -0,0 +1,25 @@
+pipeline {
+	agent any
+	options { 
+		timestamps()
+		ansiColor('xterm')
+		disableConcurrentBuilds()
+		timeout(59)
+		buildDiscarder(logRotator(numToKeepStr: '5', daysToKeepStr: '1'))
+		ws('/tmp/cicd')
+	}
+	triggers {
+		cron('@hourly')
+	}
+
+	stages {
+		stage('start') { steps { echo 'CI-PIPELINE-START' } } 
+		stage ('ci-10-main-gate') {steps {build job: 'ci-10-main-gate', parameters: [string(name: 'param1', value: "value1")]}}
+		stage ('ci-20-team-gate') {steps {build job: 'ci-20-team-gate', parameters: [string(name: 'param1', value: "value1")]}}
+		stage ('ci-30-system-gate') {steps {build job: 'ci-30-system-gate', parameters: [string(name: 'param1', value: "value1")]}}
+		stage ('ci-40-release-gate') {steps {build job: 'ci-40-system-gate', parameters: [string(name: 'param1', value: "value1")]}}
+		stage('end') { steps { echo 'CI-PIPELINE-END' } } 
+	}
+}
+
+
