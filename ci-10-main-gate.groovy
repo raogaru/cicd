308e1ef495be85010b3733386f871012dc6993fd
raogaru <raogaruinc@gmail.com>
2022-11-24 14:07:28 -0500

    Create ci-10-main-gate.groovy

---
+++ ci-10-main-gate.groovy
@@ -0,0 +1,39 @@
+pipeline {
+	agent any
+	options { 
+		timestamps()
+		ansiColor('xterm')
+		disableConcurrentBuilds()
+		timeout(59)
+		buildDiscarder(logRotator(numToKeepStr: '5', daysToKeepStr: '1'))
+		ws('/tmp/cicd')
+	}
+	environment {
+		vPROCEED = 'YES'
+	}
+
+	stages { 
+
+	stage('Main-Gate-Start') { steps { echo 'CI-PIPELINE-MAIN-GATE-START' } } 
+
+	stage('Main-Gate-Git-CICD') { steps { git(url: 'https://github.com/raogaru/cicd.git', branch: 'master', credentialsId: 'raogaru', poll: 'false') } }
+
+	stage('Main-Gate-Git-MyApp') { steps { dir('myapp') { git(url: 'https://github.com/raogaru/myapp.git', branch: 'master', credentialsId: 'raogaru', poll: 'false') } } }
+
+    stage('Main-Gate-Enter') { steps { sh './ci.sh DUMMY' } }
+
+    stage('Main-Gate-Checkin') { steps { sh './ci.sh DUMMY' } }
+
+	//stage('Main-Gate-Build') { parallel {
+			stage('Main-Gate-Build-DB-Docker') { steps { sh './ci.sh DUMMY' } }
+			stage('Main-Gate-Build-Web-Docker') { steps { sh './ci.sh DUMMY' } }
+			stage('Main-Gate-Build-App-Docker') { steps { sh './ci.sh DUMMY' } }
+	//} }
+
+	stage('Main-Gate-Verify') { steps { sh './ci.sh DUMMY' } }
+
+	stage('Main-Gate-End') { steps { echo 'CI-PIPELINE-MAIN-GATE-END' } } 
+
+	}
+}
+
