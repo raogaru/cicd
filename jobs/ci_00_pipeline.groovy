a7b593edbd6f9554a88d51967dced866db177d57
raogaru <raogaruinc@gmail.com>
2022-11-24 15:22:47 -0500

    Update ci_00_pipeline.groovy

--- jobs/ci_00_pipeline.groovy
+++ jobs/ci_00_pipeline.groovy
@@ -22,7 +22,7 @@ pipeline {
 		stage ('ci-10-main-gate') {steps {build job: 'ci-10-main-gate', parameters: [string(name: 'param1', value: "value1")]}}
 		stage ('ci-20-team-gate') {steps {build job: 'ci-20-team-gate', parameters: [string(name: 'param1', value: "value1")]}}
 		stage ('ci-30-system-gate') {steps {build job: 'ci-30-system-gate', parameters: [string(name: 'param1', value: "value1")]}}
-		stage ('ci-40-release-gate') {steps {build job: 'ci-40-system-gate', parameters: [string(name: 'param1', value: "value1")]}}
+		stage ('ci-40-release-gate') {steps {build job: 'ci-40-release-gate', parameters: [string(name: 'param1', value: "value1")]}}
 		stage('end') { steps { echo 'CI-PIPELINE-END' } } 
 	}
 }
