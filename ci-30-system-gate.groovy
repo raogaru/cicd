ba5124844892cecae2a6bdab1fbc8d2b5e223778
raogaru <raogaruinc@gmail.com>
2022-11-24 14:07:24 -0500

    Create ci-30-system-gate.groovy

---
+++ ci-30-system-gate.groovy
@@ -0,0 +1,54 @@
+pipeline {
+	agent any
+	options { 
+		timestamps()
+		ansiColor('xterm')
+		disableConcurrentBuilds()
+		timeout(59)
+		buildDiscarder(logRotator(numToKeepStr: '5', daysToKeepStr: '1'))
+		ws('/tmp/cicd')
+	}
+	environment {
+		vPROCEED = 'YES'
+	}
+
+	stages {
+		stage('System-Gate-Start') { steps { echo 'CI-PIPELINE-SYSTEM-GATE-START' } } 
+
+		stage('System-Gate-Enter') { steps { sh './ci.sh DUMMY' } }
+
+		stage('System-Gate-Build-Enter') { steps { sh './ci.sh DUMMY' } }
+
+		//stage('System-Gate-Build') { parallel {
+		stage('System-Gate-Build-DB-Docker') { steps { sh './ci.sh DUMMY' } }
+		stage('System-Gate-Build-Web-Docker') { steps { sh './ci.sh DUMMY' } }
+		stage('System-Gate-Build-App-Docker') { steps { sh './ci.sh DUMMY' } }
+		//} }
+
+		stage('System-Gate-Build-Exit') { steps { sh './ci.sh DUMMY' } }
+
+		stage('System-Gate-Deploy-Enter') { steps { sh './ci.sh DUMMY' } }
+
+		//stage('System-Gate-Deploy') { parallel {
+		stage('System-Gate-Deploy-DB-Docker') { steps { sh './ci.sh DUMMY' } }
+		stage('System-Gate-Deploy-Web-Docker') { steps { sh './ci.sh DUMMY' } }
+		stage('System-Gate-Deploy-App-Docker') { steps { sh './ci.sh DUMMY' } }
+		//} }
+
+		stage('System-Gate-Deploy-Exit') { steps { sh './ci.sh DUMMY' } }
+
+		stage('System-Gate-Test-Enter') { steps { sh './ci.sh DUMMY' } }
+
+		//stage('System-Gate-Test') { parallel {
+		stage('System-Gate-Test-Functional') { steps { sh './ci.sh DUMMY' } }
+		stage('System-Gate-Test-Performance') { steps { sh './ci.sh DUMMY' } }
+		stage('System-Gate-Test-Security') { steps { sh './ci.sh DUMMY' } }
+		//} }
+
+		stage('System-Gate-Test-Exit') { steps { sh './ci.sh DUMMY' } }
+
+		stage('System-Gate-Exit') { steps { sh './ci.sh DUMMY' } }
+
+		stage('System-Gate-End') { steps { echo 'CI-PIPELINE-SYSTEM-GATE-END' } } 
+	}
+}
