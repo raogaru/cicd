429938f8f448e1f769df0b6d88877837d9d53735
raogaru <raogaruinc@gmail.com>
2022-11-25 17:06:52 -0500

    Update ci_23_team_gate_venus.groovy

--- jobs/ci_23_team_gate_venus.groovy
+++ jobs/ci_23_team_gate_venus.groovy
@@ -14,48 +14,48 @@ pipeline {
 		ws('/tmp/cicd')
 	}
 	environment {
+		vGATE = 'TEAM'
+		vTEAM = 'VENUS'
 		vPROCEED = 'YES'
 	}
 
 	stages {
-		stage('Team-Gate-Start-VENUS') { steps { echo 'CI-PIPELINE-TEAM-GATE-VENUS-START' } } 
+		stage('Team-Gate-VENUS-Start') 		{ steps { echo 'CI-PIPELINE-TEAM-GATE-VENUS-START' } } 
 
-		stage('Team-Gate-Git-VENUS') { steps { git(url: 'https://github.com/raogaru/cicd.git', branch: 'master', credentialsId: 'raogaru', poll: 'false') } }
-		stage('Team-Gate-Enter-VENUS') { steps { sh './ci.sh DUMMY' } }
+		stage('Team-Gate-VENUS-Git') 		{ steps { 
+			git(url:'https://github.com/raogaru/cicd.git',branch:'master',credentialsId:'raogaru',poll:'false')
+			dir('myapp') {git(url:'https://github.com/raogaru/myapp.git',branch:'master',credentialsId:'raogaru',poll:'false')} 
+		} }
 
-		stage('Team-Gate-Build-Enter-VENUS') { steps { sh './ci.sh DUMMY' } }
+		stage('Team-Gate-VENUS-Enter') 		{ steps { sh './ci.sh Team-Gate-VENUS-Enter' } }
 
-		//stage('Team-Gate-Build-VENUS') { parallel {
-		stage('Team-Gate-Build-DB-Docker-VENUS') { steps { sh './ci.sh DUMMY' } }
-		stage('Team-Gate-Build-Web-Docker-VENUS') { steps { sh './ci.sh DUMMY' } }
-		stage('Team-Gate-Build-App-Docker-VENUS') { steps { sh './ci.sh DUMMY' } }
-		//} }
+		stage('Team-Gate-Build-VENUS-Enter')	{ steps { sh './ci.sh Team-Gate-Build-VENUS-Enter' } }
+		stage('Team-Gate-Build-VENUS-DB1') 	{ steps { sh './ci.sh Team-Gate-Build-VENUS-DB1' } }
+		stage('Team-Gate-Build-VENUS-DB2') 	{ steps { sh './ci.sh Team-Gate-Build-VENUS-DB2' } }
+		stage('Team-Gate-Build-VENUS-WWW') 	{ steps { sh './ci.sh Team-Gate-Build-VENUS-WWW' } }
+		stage('Team-Gate-Build-VENUS-APP1') 	{ steps { sh './ci.sh Team-Gate-Build-VENUS-APP1' } }
+		stage('Team-Gate-Build-VENUS-APP2') 	{ steps { sh './ci.sh Team-Gate-Build-VENUS-APP2' } }
+		stage('Team-Gate-Build-VENUS-APP3') 	{ steps { sh './ci.sh Team-Gate-Build-VENUS-APP3' } }
+		stage('Team-Gate-Build-VENUS-Exit') 	{ steps { sh './ci.sh Team-Gate-Build-VENUS-' } }
 
-		stage('Team-Gate-Build-Exit-VENUS') { steps { sh './ci.sh DUMMY' } }
+		stage('Team-Gate-Deploy-VENUS-Enter') 	{ steps { sh './ci.sh Team-Gate-Deploy-VENUS-Enter' } }
+		stage('Team-Gate-Deploy-VENUS-DB1') 	{ steps { sh './ci.sh Team-Gate-Deploy-VENUS-DB1' } }
+		stage('Team-Gate-Deploy-VENUS-DB2') 	{ steps { sh './ci.sh Team-Gate-Deploy-VENUS-DB2' } }
+		stage('Team-Gate-Deploy-VENUS-WWW') 	{ steps { sh './ci.sh Team-Gate-Deploy-VENUS-WWW' } }
+		stage('Team-Gate-Deploy-VENUS-APP1') 	{ steps { sh './ci.sh Team-Gate-Deploy-VENUS-APP1' } }
+		stage('Team-Gate-Deploy-VENUS-APP1') 	{ steps { sh './ci.sh Team-Gate-Deploy-VENUS-APP2' } }
+		stage('Team-Gate-Deploy-VENUS-APP3') 	{ steps { sh './ci.sh Team-Gate-Deploy-VENUS-APP3' } }
+		stage('Team-Gate-Deploy-VENUS-Exit') 	{ steps { sh './ci.sh Team-Gate-Deploy-VENUS-Exit' } }
 
-		stage('Team-Gate-Deploy-Enter-VENUS') { steps { sh './ci.sh DUMMY' } }
+		stage('Team-Gate-Test-VENUS-Enter') 	{ steps { sh './ci.sh Team-Gate-Test-VENUS-Enter' } }
+		stage('Team-Gate-Test-VENUS-Functional') { steps { sh './ci.sh Team-Gate-Test-VENUS-Functional' } }
+		stage('Team-Gate-Test-VENUS-Performance'){ steps { sh './ci.sh Team-Gate-Test-VENUS-Performance' } }
+		stage('Team-Gate-Test-VENUS-Security') 	{ steps { sh './ci.sh Team-Gate-Test-VENUS-Security' } }
+		stage('Team-Gate-Test-VENUS-Exit') 	{ steps { sh './ci.sh Team-Gate-Test-VENUS-Exit' } }
 
-		//stage('Team-Gate-Deploy-VENUS') { parallel {
-		stage('Team-Gate-Deploy-DB-Docker-VENUS') { steps { sh './ci.sh DUMMY' } }
-		stage('Team-Gate-Deploy-Web-Docker-VENUS') { steps { sh './ci.sh DUMMY' } }
-		stage('Team-Gate-Deploy-App-Docker-VENUS') { steps { sh './ci.sh DUMMY' } }
-		//} }
+		stage('Team-Gate-VENUS-Exit') { steps { sh './ci.sh Team-Gate-VENUS-Exit' } }
 
-		stage('Team-Gate-Deploy-Exit-VENUS') { steps { sh './ci.sh DUMMY' } }
-
-		stage('Team-Gate-Test-Enter-VENUS') { steps { sh './ci.sh DUMMY' } }
-
-		//stage('Team-Gate-Test-VENUS') { parallel {
-		stage('Team-Gate-Test-Functional-VENUS') { steps { sh './ci.sh DUMMY' } }
-		stage('Team-Gate-Test-Performance-VENUS') { steps { sh './ci.sh DUMMY' } }
-		stage('Team-Gate-Test-Security-VENUS') { steps { sh './ci.sh DUMMY' } }
-		//} }
-
-		stage('Team-Gate-Test-Exit-VENUS') { steps { sh './ci.sh DUMMY' } }
-
-		stage('Team-Gate-Exit-VENUS') { steps { sh './ci.sh DUMMY' } }
-
-		stage('Team-Gate-End-VENUS') { steps { echo 'CI-PIPELINE-TEAM-GATE-VENUS-END' } } 
+		stage('Team-Gate-VENUS-End') { steps { echo 'CI-PIPELINE-TEAM-GATE-VENUS' } } 
 	}
 }
       '''.stripIndent())
