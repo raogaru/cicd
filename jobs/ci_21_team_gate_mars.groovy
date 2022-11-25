d90226b142b079e7db0c6cdc37b2f6ea7dd17ea4
raogaru <raogaruinc@gmail.com>
2022-11-25 17:06:47 -0500

    Update ci_21_team_gate_mars.groovy

--- jobs/ci_21_team_gate_mars.groovy
+++ jobs/ci_21_team_gate_mars.groovy
@@ -14,48 +14,48 @@ pipeline {
 		ws('/tmp/cicd')
 	}
 	environment {
+		vGATE = 'TEAM'
+		vTEAM = 'MARS'
 		vPROCEED = 'YES'
 	}
 
 	stages {
-		stage('Team-Gate-Start-MARS') { steps { echo 'CI-PIPELINE-TEAM-GATE-MARS-START' } } 
+		stage('Team-Gate-MARS-Start') 		{ steps { echo 'CI-PIPELINE-TEAM-GATE-MARS-START' } } 
 
-		stage('Team-Gate-Git-MARS') { steps { git(url: 'https://github.com/raogaru/cicd.git', branch: 'master', credentialsId: 'raogaru', poll: 'false') } }
-		stage('Team-Gate-Enter-MARS') { steps { sh './ci.sh DUMMY' } }
+		stage('Team-Gate-MARS-Git') 		{ steps { 
+			git(url:'https://github.com/raogaru/cicd.git',branch:'master',credentialsId:'raogaru',poll:'false')
+			dir('myapp') {git(url:'https://github.com/raogaru/myapp.git',branch:'master',credentialsId:'raogaru',poll:'false')} 
+		} }
 
-		stage('Team-Gate-Build-Enter-MARS') { steps { sh './ci.sh DUMMY' } }
+		stage('Team-Gate-MARS-Enter') 		{ steps { sh './ci.sh Team-Gate-MARS-Enter' } }
 
-		//stage('Team-Gate-Build-MARS') { parallel {
-		stage('Team-Gate-Build-DB-Docker-MARS') { steps { sh './ci.sh DUMMY' } }
-		stage('Team-Gate-Build-Web-Docker-MARS') { steps { sh './ci.sh DUMMY' } }
-		stage('Team-Gate-Build-App-Docker-MARS') { steps { sh './ci.sh DUMMY' } }
-		//} }
+		stage('Team-Gate-Build-MARS-Enter')	{ steps { sh './ci.sh Team-Gate-Build-MARS-Enter' } }
+		stage('Team-Gate-Build-MARS-DB1') 	{ steps { sh './ci.sh Team-Gate-Build-MARS-DB1' } }
+		stage('Team-Gate-Build-MARS-DB2') 	{ steps { sh './ci.sh Team-Gate-Build-MARS-DB2' } }
+		stage('Team-Gate-Build-MARS-WWW') 	{ steps { sh './ci.sh Team-Gate-Build-MARS-WWW' } }
+		stage('Team-Gate-Build-MARS-APP1') 	{ steps { sh './ci.sh Team-Gate-Build-MARS-APP1' } }
+		stage('Team-Gate-Build-MARS-APP2') 	{ steps { sh './ci.sh Team-Gate-Build-MARS-APP2' } }
+		stage('Team-Gate-Build-MARS-APP3') 	{ steps { sh './ci.sh Team-Gate-Build-MARS-APP3' } }
+		stage('Team-Gate-Build-MARS-Exit') 	{ steps { sh './ci.sh Team-Gate-Build-MARS-' } }
 
-		stage('Team-Gate-Build-Exit-MARS') { steps { sh './ci.sh DUMMY' } }
+		stage('Team-Gate-Deploy-MARS-Enter') 	{ steps { sh './ci.sh Team-Gate-Deploy-MARS-Enter' } }
+		stage('Team-Gate-Deploy-MARS-DB1') 	{ steps { sh './ci.sh Team-Gate-Deploy-MARS-DB1' } }
+		stage('Team-Gate-Deploy-MARS-DB2') 	{ steps { sh './ci.sh Team-Gate-Deploy-MARS-DB2' } }
+		stage('Team-Gate-Deploy-MARS-WWW') 	{ steps { sh './ci.sh Team-Gate-Deploy-MARS-WWW' } }
+		stage('Team-Gate-Deploy-MARS-APP1') 	{ steps { sh './ci.sh Team-Gate-Deploy-MARS-APP1' } }
+		stage('Team-Gate-Deploy-MARS-APP1') 	{ steps { sh './ci.sh Team-Gate-Deploy-MARS-APP2' } }
+		stage('Team-Gate-Deploy-MARS-APP3') 	{ steps { sh './ci.sh Team-Gate-Deploy-MARS-APP3' } }
+		stage('Team-Gate-Deploy-MARS-Exit') 	{ steps { sh './ci.sh Team-Gate-Deploy-MARS-Exit' } }
 
-		stage('Team-Gate-Deploy-Enter-MARS') { steps { sh './ci.sh DUMMY' } }
+		stage('Team-Gate-Test-MARS-Enter') 	{ steps { sh './ci.sh Team-Gate-Test-MARS-Enter' } }
+		stage('Team-Gate-Test-MARS-Functional') { steps { sh './ci.sh Team-Gate-Test-MARS-Functional' } }
+		stage('Team-Gate-Test-MARS-Performance'){ steps { sh './ci.sh Team-Gate-Test-MARS-Performance' } }
+		stage('Team-Gate-Test-MARS-Security') 	{ steps { sh './ci.sh Team-Gate-Test-MARS-Security' } }
+		stage('Team-Gate-Test-MARS-Exit') 	{ steps { sh './ci.sh Team-Gate-Test-MARS-Exit' } }
 
-		//stage('Team-Gate-Deploy-MARS') { parallel {
-		stage('Team-Gate-Deploy-DB-Docker-MARS') { steps { sh './ci.sh DUMMY' } }
-		stage('Team-Gate-Deploy-Web-Docker-MARS') { steps { sh './ci.sh DUMMY' } }
-		stage('Team-Gate-Deploy-App-Docker-MARS') { steps { sh './ci.sh DUMMY' } }
-		//} }
+		stage('Team-Gate-MARS-Exit') { steps { sh './ci.sh Team-Gate-MARS-Exit' } }
 
-		stage('Team-Gate-Deploy-Exit-MARS') { steps { sh './ci.sh DUMMY' } }
-
-		stage('Team-Gate-Test-Enter-MARS') { steps { sh './ci.sh DUMMY' } }
-
-		//stage('Team-Gate-Test-MARS') { parallel {
-		stage('Team-Gate-Test-Functional-MARS') { steps { sh './ci.sh DUMMY' } }
-		stage('Team-Gate-Test-Performance-MARS') { steps { sh './ci.sh DUMMY' } }
-		stage('Team-Gate-Test-Security-MARS') { steps { sh './ci.sh DUMMY' } }
-		//} }
-
-		stage('Team-Gate-Test-Exit-MARS') { steps { sh './ci.sh DUMMY' } }
-
-		stage('Team-Gate-Exit-MARS') { steps { sh './ci.sh DUMMY' } }
-
-		stage('Team-Gate-End-MARS') { steps { echo 'CI-PIPELINE-TEAM-GATE-MARS-END' } } 
+		stage('Team-Gate-MARS-End') { steps { echo 'CI-PIPELINE-TEAM-GATE-MARS-END' } } 
 	}
 }
       '''.stripIndent())
