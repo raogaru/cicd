7f787818b5d80603e89ed732c401a700fc5bee9c
raogaru <raogaruinc@gmail.com>
2022-11-25 19:44:10 -0500

    Update ci_20_team_gate.groovy

--- jobs/ci_20_team_gate.groovy
+++ jobs/ci_20_team_gate.groovy
@@ -21,6 +21,7 @@ pipeline {
 		stage ('DEMO-CI-21-team-gate-MARS') {steps {build job: 'DEMO-CI-21-team-gate-MARS', parameters: [string(name: 'param1', value: "value1")]}}
 		stage ('DEMO-CI-22-team-gate-PLUTO') {steps {build job: 'DEMO-CI-22-team-gate-PLUTO', parameters: [string(name: 'param1', value: "value1")]}}
 		stage ('DEMO-CI-23-team-gate-VENUS') {steps {build job: 'DEMO-CI-23-team-gate-VENUS', parameters: [string(name: 'param1', value: "value1")]}}
+		stage ('DEMO-CI-24-team-gate-EARTH') {steps {build job: 'DEMO-CI-24-team-gate-EARTH', parameters: [string(name: 'param1', value: "value1")]}}
 		} }
 		stage('end') { steps { echo 'CI-PIPELINE-TEAM-GATE-END' } } 
 	}
