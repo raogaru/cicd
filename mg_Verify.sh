99adba7385fb10cc22e2ddcc6a90fb8e162e3932
raogaru <raogaruinc@gmail.com>
2022-11-26 22:24:05 -0500

    Update mg_Verify.sh

--- mg_Verify.sh
+++ mg_Verify.sh
@@ -2,6 +2,13 @@
 MARKER "script:mg_Verify.sh START"
 # ######################################################################
 echo arguments $* 
+
+HEADER2 "Verify Dockers"
+ADDENV "MAIN_VERIFY_DOCKER=PASS"
+
+HEADER2 "Verify Databases"
+ADDENV "MAIN_VERIFY_DB=PASS"
+
 # ######################################################################
 MARKER "script:mg_Verify.sh END"
 # ######################################################################
